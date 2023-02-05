e541a494547e03b6b3d7e86d894331c3738bb042
BobJay77 <farhanirani77@gmail.com>
2023-02-05 06:01:33 -0500

    Player Movement added

--- TheThirdFish/Assets/Scripts/Player.cs
+++ TheThirdFish/Assets/Scripts/Player.cs
@@ -4,18 +4,100 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public ReplacementShaderEffect shaderEffect;
+    public CharacterController characterController;
+    public bool isGrounded;
+    public LayerMask groundLayer;
+    
+    public Vector2 inputMovementVector;
+    public GameObject mainCamera;
 
+    public float speed = 10;
+    public float verticalVelocity = 0;
+    public float gravity = -15.0f;
+
+    public float sensitivity = 10;
+
+    public float health = 10;
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        GroundedChecker(); //quick groundcheck
+        GravityHandler();
+        InputHandler();
+        FinalMovementHandler();
+    }
+
+    private void LateUpdate()
+    {
+        CameraMovement(); 
+        inputMovementVector = Vector2.zero;
+        speed = 0;
+       
+    }
+
+    private void GravityHandler()
+    {
+        if (!isGrounded)
         {
-            shaderEffect.enabled = true;
+            verticalVelocity += gravity * Time.deltaTime;
         }
         else
         {
-            shaderEffect.enabled = false;
-            shaderEffect.reset = true;
+            verticalVelocity = 0;
         }
     }
+
+    private void InputHandler()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputMovementVector.y = 1;
+            speed = 10;
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            inputMovementVector.y = -1;
+            speed = 10;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputMovementVector.x = -1;
+            speed = 10;
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            inputMovementVector.x = 1;
+            speed = 10;
+        }
+
+    }
+
+    private void FinalMovementHandler()
+    {
+        Vector3 inputDir = new Vector3(inputMovementVector.x, 0, inputMovementVector.y);
+        float targetRotation = Mathf.Atan2(inputDir.x, inputDir.z) * Mathf.Rad2Deg + mainCamera.transform.eulerAngles.y;
+        Vector3 targetMotion = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
+
+        characterController.Move(targetMotion.normalized * (speed * Time.deltaTime) + new Vector3(0.0f, verticalVelocity, 0.0f) * Time.deltaTime);
+
+    }
+
+    private void GroundedChecker()
+    {
+        Vector3 spherePos = new Vector3(transform.position.x, (characterController.center.y + transform.position.y) - -.14f - characterController.height / 2.6f,
+                            transform.position.z);
+        isGrounded = Physics.CheckSphere(spherePos, characterController.radius, groundLayer,
+                     QueryTriggerInteraction.Ignore);
+
+    }
+
+    private void CameraMovement()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(Vector3.up, mouseX * sensitivity);
+    }
+
+   
 }
